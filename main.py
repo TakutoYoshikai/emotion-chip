17576422bcb7bba238de3a9692133ce4e7277981
takuto <takuto.yoshikai@gmail.com>
2020-05-18 23:55:19 +0900

    tmp

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
+docvec_size = 10
 x = tf.placeholder(tf.float32, [1, 5])
-w = tf.placeholder(tf.float32, [1, 10])
-W2 = tf.Variable(tf.zeros([10, 5], tf.float32))
+w = tf.placeholder(tf.float32, [1, docvec_size])
+W2 = tf.Variable(tf.zeros([docvec_size, 5], tf.float32))
 W1 = tf.matmul(w, W2)
 b = tf.Variable(tf.zeros([5]))
 y = tf.sigmoid(x * W1 + b)
